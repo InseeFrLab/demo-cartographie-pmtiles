5616cddfb25fbb6db2ffc9d9ca726873dbec0b30
ggj62i <cedric.bobinec@insee.fr>
2025-11-28 15:01:19 +0000

    doc

--- R/1_preparer_pmtiles.R
+++ R/1_preparer_pmtiles.R
@@ -12,6 +12,7 @@ url_data <- "https://www.insee.fr/fr/statistiques/fichier/8647014/base-ic-evol-s
 chemin_donnees <- "data/raw/base-ic-evol-struct-pop-2022.CSV"
 
 # Iris GE 2022 - https://geoservices.ign.fr/irisge
+# TODO Ici France métro uniquement, adapter pour ajouter les DOM (projections différentes à gérer)
 url_geo <- "https://data.geopf.fr/telechargement/download/IRIS-GE/IRIS-GE_2-0__SHP__FRA_2022-01-01/IRIS-GE_2-0__SHP__FRA_2022-01-01.7z"
 chemin_shapefile_geopackage <- "data/raw/IRIS-GE_2-0__SHP__FRA_2022-01-01/IRIS-GE/1_DONNEES_LIVRAISON_2022-05-00267/IRIS-GE_2-0_SHP_LAMB93_FXX-2022/IRIS_GE.SHP"
 
@@ -46,10 +47,10 @@ archive::archive_extract(path_7z_geo,
 geo <- st_read(chemin_shapefile_geopackage)
 
 #au besoin afficher les 5 premières ligne du fichier
-#View(fm[1:5,])
+#View(geo[1:5,])
 
 #projeter en wgs 84 (code crs=4326) la géométrie (https://fr.wikipedia.org/wiki/WGS_84)
-fm <- st_transform(geo, crs = 4326)
+geo <- st_transform(geo, crs = 4326)
 st_crs(geo)
 
 # au besoin : export de la table au format geojson 
@@ -61,7 +62,7 @@ st_crs(geo)
 
 # Au besoin afficher les iris de loire atlantique. A noter que la base est assez lourde.
 # Par exemple si on veut afficher les iris des pays de la loire, R a du mal à suivre. Il faut être patient.
-# plot(fm[which(substr(fm$CODE_IRIS,1,2) %in% c("44")),1])
+# plot(geo[which(substr(geo$CODE_IRIS,1,2) %in% c("44")),1])
 
 #import des données et sélection des variables utiles
 donnees <- read.csv2(chemin_donnees, dec=".") |> 
@@ -72,7 +73,7 @@ donnees <- read.csv2(chemin_donnees, dec=".") |>
 #part des retraités parmi les 15 ans ou plus
 donnees$part_retraite_15p <- round(donnees$C22_POP15P_STAT_GSEC32/donnees$C22_POP15P*100, 2)
 
-#fusion des données (dans la table donnees) et de la géographie (dans la table fm)
+#fusion des données (dans la table donnees) et de la géographie (dans la table geo)
 geo_avec_donnees <- merge(geo,
                           donnees,
                           by.x="CODE_IRIS",
@@ -82,7 +83,13 @@ geo_avec_donnees <- merge(geo,
                         )
 
 #export du fichier geojson
-st_write(geo_avec_donnees, chemin_geojson_avec_donnees)
+st_write(
+  obj = geo_avec_donnees, 
+  dsn = chemin_geojson_avec_donnees,
+  # écrase si existe déjà
+  append = FALSE, 
+  delete_dsn = TRUE
+)
 
 # générer un fichier tuilé avec l'outil tippecanoe
 
@@ -92,10 +99,11 @@ st_write(geo_avec_donnees, chemin_geojson_avec_donnees)
 ## --layer= : à noter car à reprendre ensuite dans la visualisation : nom de la couche dans le fichier tuilé, il contient la géométrie + les indicateurs
 ## --no-tile-size-limit ne limite pas la taille des tuiles
 ## --force : écrase le fichier pmtiles s'il existe déjà
+## TODO A ajuster pour optimiser la taille des tuiles
 ## --drop-rate=0, --no-feature-limit et --no-tile-size-limit sont des paramètres qui limitent au maximum la simplification des polygones, on aura ici des polygones bien définis, ce qui peut parfois entrainer des lenteur sur la carte
 
 ## la fonction system de R permet d'exécuter à partir d'un script R des commandes dans un terminal
-## et le package glue facilite l'injection des paramètres et et les sauts de lignes
+## et le package glue facilite l'injection des paramètres et les sauts de lignes
 
 system(
   glue(
@@ -105,7 +113,7 @@ system(
     "--force ",
     "--drop-rate=0 ",
     "--no-feature-limit ",
-    "--no-tile-size-limit ",
+    # "--no-tile-size-limit ",
     "--layer=data"
   )
 )
